d001069580433b5e93d703ed9542a3dc6bebf866
Michael Savage <michaelsavage940@gmail.com>
2025-12-14 12:48:40 +0100

    add user recipient admin view

--- recipients/admin.py
+++ recipients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from recipients.models import UserRecipient
 
-# Register your models here.
+
+@admin.register(UserRecipient)
+class UserRecipientAdmin(admin.ModelAdmin):
+    list_display = ["email", "name", "user", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["email", "name", "user__email"]
+    raw_id_fields = ["user"]
