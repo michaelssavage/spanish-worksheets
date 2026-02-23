05fb49189e3d29099a38a98e99b6c2b7783efa35
Michael Savage <michaelsavage940@gmail.com>
2026-02-23 12:02:18 +0100

    remove

--- config/settings.py
+++ config/settings.py
@@ -13,14 +13,8 @@ REDIS_URL = config("UPSTASH_REDIS_URL")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config("SETTINGS_SECRET_KEY")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z*216t$rs4qzo=-2#=*=^v7or(*ln(05#ap63n!^ft8=nj^9$4"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 raw_hosts = config("ALLOWED_HOSTS", default="localhost,127.0.0.1")
 
